c72930fdb207b6f96f6f171d8484e39a614f2a3f
tasim313 <mostasimmahmudtasim@gmail.com>
2021-03-21 07:04:14 -0400

    Edit forms and create Add_student_Form,EditStudentForm(

--- student_management_app/forms.py
+++ student_management_app/forms.py
@@ -1,47 +1,72 @@
 from django import forms
-from django.forms import ChoiceField
-from .models import Courses, Subjects, Students, SessionYearModel
 
-
-class ChoiceNoValidation(ChoiceField):
-    def validate(self, value):
-        pass
+from .models import Courses
 
 
 class DateInput(forms.DateInput):
-    input_type = 'date'
+    input_type = "date"
 
 
 class AddStudentForm(forms.Form):
-    email = forms.EmailField(label='Email', max_length=50,
-                             widget=forms.EmailInput(
-                                 attrs={'class': 'form-control', "autocomplete": "off"}))
-    password = forms.CharField(label='Password', max_length=50,
-                               widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(label='First Name', max_length=30,
-                                 widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(label='Last Name', max_length=30,
-                                widget=forms.TextInput(attrs={'class': 'form-control'}))
-    username = forms.CharField(label='Username', max_length=50,
-                               widget=forms.TextInput(attrs={'class': 'form-control'}))
-    address = forms.CharField(label="Address", max_length=200,
-                              widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label="Email", max_length=50,
+                             widget=forms.EmailInput(attrs={"class": "form-control"}))
+    password = forms.CharField(label="Password", max_length=50,
+                               widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    first_name = forms.CharField(label="First Name", max_length=50,
+                                 widget=forms.TextInput(attrs={"class": "form-control"}))
+    last_name = forms.CharField(label="Last Name", max_length=50,
+                                widget=forms.TextInput(attrs={"class": "form-control"}))
+    username = forms.CharField(label="Username", max_length=50,
+                               widget=forms.TextInput(attrs={"class": "form-control"}))
+    address = forms.CharField(label="Address", max_length=50,
+                              widget=forms.TextInput(attrs={"class": "form-control"}))
+
+    courses = Courses.objects.all()
     course_list = []
+    for course in courses:
+        small_course = (course.id, course.course_name)
+        course_list.append(small_course)
 
-    try:
-        courses = Courses.objects.all()
-        for course in courses:
-            small_course = (course.id, course.course_name)
-            course_list.append(small_course)
-    except:
-        course_list = []
+    gender_choice = (
+        ("Male", "Male"),
+        ("Female", "Female")
+    )
 
-    session_list = []
-    try:
-        sessions = SessionYearModel.object.all()
+    course = forms.ChoiceField(label="Course", choices=course_list,
+                               widget=forms.Select(attrs={"class": "form-control"}))
+    sex = forms.ChoiceField(label="Sex", choices=gender_choice,
+                            widget=forms.Select(attrs={"class": "form-control"}))
+    session_start = forms.DateField(label="Session Start", widget=DateInput(attrs={"class": "form-control"}))
+    session_end = forms.DateField(label="Session End", widget=DateInput(attrs={"class": "form-control"}))
+    profile_pic = forms.FileField(label="Profile Pic", max_length=50,
+                                  widget=forms.FileInput(attrs={"class": "form-control"}))
 
-        for ses in sessions:
-            small_ses = (ses.id, str(ses.session_start_year) + "   TO  " + str(ses.session_end_year))
-            session_list.append(small_ses)
-    except:
-        session_list = []
+
+class EditStudentForm(forms.Form):
+    email = forms.EmailField(label="Email", max_length=50,
+                             widget=forms.EmailInput(attrs={"class": "form-control"}))
+    first_name = forms.CharField(label="First Name", max_length=50,
+                                 widget=forms.TextInput(attrs={"class":"form-control"}))
+    last_name = forms.CharField(label="Last Name", max_length=50,
+                                widget=forms.TextInput(attrs={"class": "form-control"}))
+    username = forms.CharField(label="Username", max_length=50,
+                               widget=forms.TextInput(attrs={"class": "form-control"}))
+    address = forms.CharField(label="Address", max_length=50,
+                              widget=forms.TextInput(attrs={"class": "form-control"}))
+
+    courses = Courses.objects.all()
+    course_list=[]
+    for course in courses:
+        small_course = (course.id,course.course_name)
+        course_list.append(small_course)
+
+    gender_choice=(
+        ("Male", "Male"),
+        ("Female", "Female")
+    )
+
+    course = forms.ChoiceField(label="Course", choices=course_list, widget=forms.Select(attrs={"class": "form-control"}))
+    sex=forms.ChoiceField(label="Sex", choices=gender_choice, widget=forms.Select(attrs={"class": "form-control"}))
+    session_start = forms.DateField(label="Session Start", widget=DateInput(attrs={"class": "form-control"}))
+    session_end = forms.DateField(label="Session End", widget=DateInput(attrs={"class":"form-control"}))
+    profile_pic = forms.FileField(label="Profile Pic", max_length=50, widget=forms.FileInput(attrs={"class": "form-control"}), required=False)
